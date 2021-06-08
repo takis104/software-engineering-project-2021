648557102b4912deb91d847273b05ed0ec62cd7c
Christos Balatsouras <52033650+takis104@users.noreply.github.com>
2021-06-09 02:04:38 +0300

    Revert "Add files via upload"
    This reverts commit b112483ba79fe6e35d6da2aafa614cdedaa73fa4.

--- Project Code/Teacher.java
+++ Project Code/Teacher.java
@@ -66,50 +66,46 @@ public class Teacher extends JDialog {
 		gbc2.gridx = 0;  gbc2.gridy = 0; gbc2.gridwidth=2;
 		screen.add(lbl1, gbc2);
 		
-		JButton btn1 = Cval.AddButton(screen, 1, 1, "/images/mn_im10.png", "Αποστολή μηνύματος");
+		JButton btn1 = Cval.AddButton(screen, 1, 1, "/images/mn_im13.png", "Αποστολή μηνύματος");
 		JButton btn2 = Cval.AddButton(screen, 1, 2, "/images/mn_im07.png", "Ανάθεση εργασίας");
 		JButton btn3 = Cval.AddButton(screen, 1, 3, "/images/mn_im16.png", "Οι εργασίες που έχω αναθέσει");
-		JButton btn4 = Cval.AddButton(screen, 2, 3, "/images/mn_im05.png", "Ανακοινώσεις");
-		JButton btn5 = Cval.AddButton(screen, 2, 1, "/images/mn_im02.png", "Εισερχόμενα");
-		JButton btn6 = Cval.AddButton(screen, 2, 2, "/images/mn_im06.png", "Απεσταλμένα");
+		JButton btn4 = Cval.AddButton(screen, 2, 3, "/images/mn_im03.png", "Ανακοινώσεις");
+		JButton btn5 = Cval.AddButton(screen, 2, 1, "/images/mn_im18.png", "Εισερχόμενα");
+		JButton btn6 = Cval.AddButton(screen, 2, 2, "/images/mn_im19.png", "Απεσταλμένα");
 		
 		btn1.addActionListener(new ActionListener() { //new message
 			public void actionPerformed(ActionEvent arg0) {
-				new MessageFx(Cval.ScreenWidth, Cval.ScreenHeight, "Μήνυμα",null, 0);
+				newMessage();
 			}
 		});
 		
 		btn2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) { //new project
-				new MessageFx(Cval.ScreenWidth, Cval.ScreenHeight, "Νέα εργασία",null, 1);
+				newProject();
 			}
 		});
 		
-		btn3.addActionListener(new ActionListener() { //old project
+		btn3.addActionListener(new ActionListener() { //old projects
 			public void actionPerformed(ActionEvent arg0) {
-				Cval.id_from_parent.push(db_interface.user_id);
-				sql_from_parent = "SELECT DISTINCT(m.id) AS Κωδικός, m.msg_date AS Ημερομηνία, m.msg_subject AS Θέμα, m.deadline as Προθεσμία, m.cloud_id AS link FROM msgs as m INNER JOIN msgs_details as md WHERE deadline is not null AND md.from_user_id = " + db_interface.user_id;
-				new MultirowForm("Εργασίες που ανέθεσα", sql_from_parent, true, true, true, Cval.OPEN_MULTIROW); //last true = call view message on edit
+				showProjects();
 			}
 		});
 		
 		btn4.addActionListener(new ActionListener() { //new announcement
 			public void actionPerformed(ActionEvent arg0) {
-				new MessageFx(Cval.ScreenWidth, Cval.ScreenHeight, "Ανακοίνωση",null, 2);
+				manageAnnouncements();
 			}
 		});
 		
 		btn5.addActionListener(new ActionListener() { //Incomimg msgs
 			public void actionPerformed(ActionEvent arg0) {
-				sql_from_parent = "SELECT m.id AS Κωδικός, m.msg_date AS Ημερομηνία, m.msg_subject AS Θέμα, CONCAT(u.surname, ' ', u.firstname) as Αποστολέας, m.cloud_id as online_id, u.id As sender_id FROM msgs as m INNER JOIN msgs_details as md on m.id= md.msg_id INNER JOIN users as u on md.from_user_id=u.id WHERE kind=0 and md.to_user_id =" + db_interface.user_id + " ORDER BY msg_date desc";
-				new MultirowForm("Εισερχόμενα", sql_from_parent, false, true, true, Cval.OPEN_EDITOR);
+				showIncomingMessages();
 			}
 		});
 
 		btn6.addActionListener(new ActionListener() { //Outgoing msgs
 			public void actionPerformed(ActionEvent arg0) {
-				sql_from_parent = "SELECT distinct(m.id) AS Κωδικός, m.msg_date AS Ημερομηνία, m.msg_subject AS Θέμα, m.cloud_id as online_id FROM msgs as m INNER JOIN msgs_details as md on m.id= md.msg_id WHERE kind=0 and md.from_user_id = " + db_interface.user_id + " ORDER BY msg_date desc";
-				new MultirowForm("Απεσταλμένα", sql_from_parent, true, true, true,  Cval.OPEN_EDITOR);
+				showOutgoingMessages();
 			}
 		});
 			
@@ -118,7 +114,10 @@ public class Teacher extends JDialog {
 		exit_btn.setMaximumSize(new Dimension(35, 35));
 		exit_btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				new Mrat();
+				if (JOptionPane.showConfirmDialog(null, "Θέλετε να αξιολογήσετε την εφαρμογή", "Αξιολόγηση",
+				        JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+					new Feedback();
+				}
 				screen.dispose();
 			}
 		});
@@ -130,10 +129,37 @@ public class Teacher extends JDialog {
         //screen.setJMenuBar(PrepareMenu());
 		
 		screen.setVisible(true);
-
 	}
 	
-	private JMenuBar PrepareMenu() {
+	public void newMessage() {
+		new MessageFx(Cval.ScreenWidth, Cval.ScreenHeight, "Μήνυμα",null, 0);
+	}
+	
+	public void newProject() {
+		new MessageFx(Cval.ScreenWidth, Cval.ScreenHeight, "Νέα εργασία",null, 1);
+	}
+	
+	public void showProjects() {
+		Cval.id_from_parent.push(db_interface.user_id);
+		sql_from_parent = "SELECT DISTINCT(m.id) AS Κωδικός, m.msg_date AS Ημερομηνία, m.msg_subject AS Θέμα, m.deadline as Προθεσμία, m.cloud_id AS link FROM msgs as m INNER JOIN msgs_details as md WHERE deadline is not null AND md.from_user_id = " + db_interface.user_id;
+		new MultirowForm("Εργασίες που ανέθεσα", sql_from_parent, true, true, true, Cval.OPEN_MULTIROW); //last true = call view message on edit
+	}
+	
+	public void manageAnnouncements() {
+		new MessageFx(Cval.ScreenWidth, Cval.ScreenHeight, "Ανακοίνωση",null, 2);
+	}
+	
+	public void showIncomingMessages() {
+		sql_from_parent = "SELECT m.id AS Κωδικός, m.msg_date AS Ημερομηνία, m.msg_subject AS Θέμα, CONCAT(u.surname, ' ', u.firstname) as Αποστολέας, m.cloud_id as online_id, u.id As sender_id FROM msgs as m INNER JOIN msgs_details as md on m.id= md.msg_id INNER JOIN users as u on md.from_user_id=u.id WHERE kind=0 and md.to_user_id =" + db_interface.user_id + " ORDER BY msg_date desc";
+		new MultirowForm("Εισερχόμενα", sql_from_parent, false, true, true, Cval.OPEN_EDITOR);
+	}
+	
+	public void showOutgoingMessages() {
+		sql_from_parent = "SELECT distinct(m.id) AS Κωδικός, m.msg_date AS Ημερομηνία, m.msg_subject AS Θέμα, m.cloud_id as online_id FROM msgs as m INNER JOIN msgs_details as md on m.id= md.msg_id WHERE kind=0 and md.from_user_id = " + db_interface.user_id + " ORDER BY msg_date desc";
+		new MultirowForm("Απεσταλμένα", sql_from_parent, true, true, true,  Cval.OPEN_EDITOR);
+	}
+	
+	/*private JMenuBar PrepareMenu() {
 	    // create a menubar
 		JMenuBar mb = new JMenuBar();
 	
@@ -159,5 +185,5 @@ public class Teacher extends JDialog {
 	    // add menu to menu bar
 	   	mb.add(m1);
 	   	return mb;
-	}
+	}*/
 }
